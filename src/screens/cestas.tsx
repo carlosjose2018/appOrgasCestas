7b8948a31aa6525f0704524d689516741ebe43f8
José Carlos Araujo <carlosaraujo10jose3000@gmail.com>
2022-12-26 13:04:40 -0300

    intall project

--- src/screens/cestas.tsx
+++ src/screens/cestas.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image, StyleSheet, Dimensions, Text } from 'react-native';
 
+import topo from "../assets/topo.png"
+
+const width = Dimensions.get('screen').width;
 
 export function Cestas() {
   return (
-    <Text>
-       cestas da moeda
-    </Text>
+  <>
+      <Image source={topo} style={topoImg.topStyle}/>
+      <Text style={topoImg.textDescription}>Detalhes da Cestas</Text>
+  </>   
   );
 }
+
+const topoImg = StyleSheet.create({
+    topStyle:{
+      width:"100%",
+      height: 578 / 768 * width,
+    },
+    textDescription:{
+      width:"100%",
+      position:"absolute",
+      textAlign:"center"
+    }
+})
